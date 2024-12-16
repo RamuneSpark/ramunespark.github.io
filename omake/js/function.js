217161cee674d22b18335a5c517803610bfd00b8
RamuneSpark-Sugedama01 <120969218+RamuneSpark-Sugedama01@users.noreply.github.com>
2024-12-17 03:19:46 +0900

    update

---
+++ omake/js/function.js
@@ -0,0 +1,39 @@
+const omakeBox = document.getElementById("omakeBox");
+omakeBox.style.display = "block";
+
+const omakeBox2 = document.getElementById("omakeBox2");
+omakeBox2.style.display = "block";
+
+const ramuneBlue = "#1959A8";
+const ramuneBlue2 = "#5D98B5";
+const ramuneYellow = "#F2EE6C";
+const ramunePink = "#DA88B6";
+
+function createManual(e){
+
+    const p = document.createElement("p");
+    omakeBox2.appendChild(p)
+    p.textContent = e;
+
+}
+
+function createDiv(e){
+
+    const div = document.createElement("div");
+    div.id = e;
+    return div;
+
+}
+
+function change_0or1(e){
+
+    let a;
+    if(e){
+        a = 1;
+    }else{
+        a = 0;
+    }
+
+    return a;
+
+}
